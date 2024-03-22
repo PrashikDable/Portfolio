45f2323332b71bf7e4d20882032d8b45bf4b8a40
PrashikDable <prashikdable7@gmail.com>
2024-03-22 16:04:48 +0530

    Resume Update

--- src/components/SocialLinks.js
+++ src/components/SocialLinks.js
@@ -40,7 +40,7 @@ const SocialLinks = () => {
           Resume <BsFillPersonLinesFill size={30} />
         </>
       ),
-      href: "/Resume.pdf",
+      href: "/PRASHIK DABLE Resume.pdf",
       style: "rounded-br-md",
       download: true,
     },
